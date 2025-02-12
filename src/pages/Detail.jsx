23b5e495ad03e15aec55386145044ae9c42eb70b
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2025-02-12 22:59:07 +0530

    feat: dummy screen

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useParams } from "react-router-dom";
+
+const BookDetailPage = () => {
+  
+  return (
+    <>
+    book detail
+    </>
+  );
+};
+
+export default BookDetailPage;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const HomePage = () => {
+  
+  return (
+    <div className="container mt-5">
+      home
+    </div>
+  );
+};
+
+export default HomePage;
