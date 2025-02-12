7bc116c9bd2aedcc929943c1fc2ec36136a676d8
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2025-02-12 22:59:21 +0530

    fix: routing added

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import { Routes, Route } from "react-router-dom";
 // CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import MyNavbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      {/* <MyNavbar />
+      <MyNavbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/book/list" element={<ListingPage />} />
+        {/* <Route path="/book/list" element={<ListingPage />} />
         <Route path="/book/view/:bookId" element={<BookDetailPage />} />
         <Route path="/book/orders" element={<OrdersPage />} />
-        <Route path="/books/orders/:bookId" element={<ViewOrderDetails />} />
-      </Routes> */}
-      hi
+        <Route path="/books/orders/:bookId" element={<ViewOrderDetails />} /> */}
+      </Routes>
     </div>
   );
 }
