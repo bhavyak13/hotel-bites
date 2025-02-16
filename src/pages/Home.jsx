eb5db36916a806908570d0c0464987dc80eb38d7
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2025-02-16 14:33:22 +0530

    feat: home screen with listing all pages implementation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from "react";
+import CardGroup from "react-bootstrap/CardGroup";
+
+import { useFirebase } from "../context/Firebase";
+import FoodCard from "../components/Card";
 
 const HomePage = () => {
-  
+  const firebase = useFirebase();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    firebase.getDocuments()
+      .then((data) =>
+        setData(data.docs)
+      );
+  }, []);
+
   return (
     <div className="container mt-5">
-      home
+      <CardGroup>
+        {data?.map((book) => (
+          <FoodCard
+            link={`/book/view/${book.id}`}
+            key={book.id}
+            id={book.id}
+            {...book.data()}
+          />
+        ))}
+      </CardGroup>
     </div>
   );
 };
